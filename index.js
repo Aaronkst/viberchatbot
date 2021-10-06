e87a42c0cd4c02f5f3d44020417cda1e5e481d52
Aaronkst <aaron.kst99@gmail.com>
2021-10-06 15:08:04 +0630

    Logo Fix
    Attempt 1 #
    - Added path to image
    - Added url to avatar

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 const express = require('express');
 const app = express();
-const botlogo = require('./img/chatbot.jpg');
 const viberMessage = require('./functions/viberMessage');
 
 const bot = new ViberBot({
     authToken: "4e1523bd2527d7fe-aca763d2f53b468e-babfba2d4e53f5f9",
-    name: "M-Points",
-    avatar: botlogo
+    name: "Aaron Chatbot",
+    avatar: "https://viber-bot-aaron.herokuapp.com/logo"
 });
 
 app.listen(process.env.PORT || 3000, () => {
@@ -26,12 +25,16 @@ app.post('/setWebhook', async(req,res)=>{
     }
 });
 
+app.get('/logo', (req,res)=>{
+    res.status(200).sendFile('./img/chatbot.jpg');
+});
+
 app.get('/', (req,res)=>{
-    res.status(200).send('Server is running')
+    res.status(200).send('Server is running');
 });
 
 bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFinish) => {
     console.log(userProfile);
     console.log(context);
     //let message = viberMessage.convoStart(context, userProfile.userProfile.name);
-})
+});
